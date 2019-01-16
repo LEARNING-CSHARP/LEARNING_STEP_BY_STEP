fbe82e9abef98652c6bb3c9e076ac3ac300e670c
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-01-16 15:02:56 +0330

    Add project files.

---
+++ LEARNING_STEP_BY_STEP/Program.cs
@@ -0,0 +1,608 @@
+﻿//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			System.Console.WriteLine("Hello, World");
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//using System;
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			Console.WriteLine("Hello, World");
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//using System;
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			Console.WriteLine("Hello, World");
+
+//			Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int x;
+
+//			int y;
+
+//			// y = x + 1; // Assignment!
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int x;
+//			x = 1; // Initial Value
+
+//			System.Console.WriteLine(x);
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int x = 1;
+
+//			System.Console.WriteLine(x);
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			// تاکتیک
+//			//int x = 1;
+//			int firstNumber = 1;
+
+//			System.Console.WriteLine(firstNumber);
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			string fullName = "Mr. Dariush Tasdighi";
+
+//			System.Console.WriteLine(fullName);
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			//int firstNumber = System.Console.ReadLine();
+
+//			string fullName = System.Console.ReadLine();
+
+//			System.Console.WriteLine(fullName);
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			System.Console.WriteLine("What is your full name:");
+
+//			string fullName = System.Console.ReadLine();
+
+//			System.Console.WriteLine(fullName);
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			System.Console.Write("What is your full name: ");
+
+//			string fullName = System.Console.ReadLine();
+
+//			System.Console.WriteLine(fullName);
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			System.Console.Write("What is your first name: ");
+//			string firstName = System.Console.ReadLine();
+
+//			System.Console.Write("What is your last name: ");
+//			string lastName = System.Console.ReadLine();
+
+//			//string fullName = firstName + lastName;
+
+//			//string fullName = firstName + "" + lastName;
+
+//			//string fullName = firstName + " " + lastName;
+
+//			//string fullName = "Full Name: " + firstName + " " + lastName;
+
+//			//string fullName = string.Format("Full Name: {0} {1}", firstName, lastName);
+
+//			//string fullName = string.Format("Full Name: {1} {0}", lastName, firstName);
+
+//			//string fullName = $"Full Name: {firstName} {lastName}";
+
+//			//string fullName = $"Full Name: { firstName } { lastName }";
+
+//			string fullName =
+//				$"Full Name: { firstName } { lastName }";
+
+//			System.Console.WriteLine(fullName);
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int A = 10;
+//			int B = 20;
+//			int C = 30;
+
+//			string result = string.Format("M = {1}, N = {0}, P = {1}", A, B, C);
+
+//			System.Console.WriteLine(result);
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int index = 1;
+
+//			if (index <= 10)
+//			{
+//				System.Console.WriteLine("Hello, World!");
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int index = 1;
+
+//			while (index <= 10)
+//			{
+//				System.Console.WriteLine("Hello, World!");
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int index = 1;
+
+//			while (index <= 10)
+//			{
+//				System.Console.WriteLine("Hello, World!");
+
+//				// جمله عادی
+//				// مقداری که در داخل ایندکس وجود دارد
+//				// را با مقدار عددی یک جمع کرده و
+//				// حاصل آنرا مجددا در همان متغیر ایندکس ذخیره می‌کنیم
+
+//				// جمله قشنگ و عامیانه
+//				// یک واحد به متغیر ایندکس اضافه می‌کند
+//				index = index + 1;
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			// وقتی بخواهیم
+//			// ده بار دستور یا دستوراتی را انجام دهیم
+//			int index = 1;
+
+//			while (index <= 10)
+//			{
+//				// Do Something(s)
+
+//				index = index + 1;
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int A = 10;
+
+//			A = A + 20;
+//			A += 20;
+
+//			A = A + 1;
+//			A += 1;
+//			A++;
+//			++A;
+
+//			A = A - 1;
+//			A -= 1;
+//			A--;
+//			--A;
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int index = 1;
+
+//			while (index <= 10)
+//			{
+//				// Do Something(s)
+
+//				index++;
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			//for( A ; B ; C )
+//			//{
+//			//	D
+//			//}
+//			// Run: A, B, D, C, B, D, C,...
+
+//			for (int index = 1; index <= 10; index++)
+//			{
+//				System.Console.WriteLine("Hello, World!");
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int number = 10;
+
+//			for (int index = 1; index <= number; index++)
+//			{
+//				System.Console.WriteLine(index);
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			//int A;
+//			//int A;
+
+//			//int A;
+//			//string A;
+
+//			int firstNumber;
+//			int firstNumberInt;
+
+//			string fullName;
+//			string fullNameString;
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			System.Console.Write("choose a number: ");
+//			string numberString = System.Console.ReadLine();
+
+//			int number =
+//				System.Convert.ToInt32(numberString);
+
+//			int sum = 0;
+
+//			for (int index = 1; index <= number; index++)
+//			{
+//				sum += index;
+//			}
+
+//			string result =
+//				$"The sum of these numbers is { sum }";
+
+//			System.Console.WriteLine(result);
+
+//			//System.Console.WriteLine("Press [ENTER] To Exit... ");
+
+//			//System.Console.Write("Press [ENTER] To Exit... ");
+
+//			//System.Console.Write("\r\nPress [ENTER] To Exit... ");
+
+//			//System.Console.Write("\r\n\r\nPress [ENTER] To Exit... ");
+
+//			//System.Console.WriteLine();
+//			//System.Console.WriteLine();
+//			//System.Console.Write("Press [ENTER] To Exit... ");
+
+//			//System.Console.Write
+//			//	(System.Environment.NewLine + System.Environment.NewLine + "Press [ENTER] To Exit... ");
+
+//			System.Console.Write
+//				(System.Environment.NewLine + "Press [ENTER] To Exit... ");
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int A = 10;
+//			int B = 20;
+
+//			int C = ((A + B) - 1) * A - B * 10;
+
+//			System.Console.WriteLine(C);
+
+//			//...
+
+//			int M = 22;
+//			int N = 17;
+
+//			int P = ((M + N) - 1) * M - N * 10;
+
+//			System.Console.WriteLine(P);
+
+//			//...
+
+//			int L = 22;
+//			int S = 17;
+
+//			int W = ((L + M) - 1) * L - S * 10;
+
+//			System.Console.WriteLine(P);
+
+//			System.Console.Write
+//				(System.Environment.NewLine + "Press [ENTER] To Exit... ");
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+namespace LEARNING_STEP_BY_STEP
+{
+	class Program
+	{
+		static int DoSomething(int x, int y)
+		{
+			//int result = ((x + y) - 1) * x - y * 10;
+			int result = ((x + y) - 1) * x - y * 11;
+
+			return result;
+		}
+
+		static void Main(string[] args)
+		{
+			int A = 10;
+			int B = 20;
+
+			int C = DoSomething(A, B);
+			//int C = ((A + B) - 1) * A - B * 10;
+
+			System.Console.WriteLine(C);
+
+			//...
+
+			int M = 22;
+			int N = 17;
+
+			int P = DoSomething(M, N);
+			//int P = ((M + N) - 1) * M - N * 10;
+
+			System.Console.WriteLine(P);
+
+			//...
+
+			int L = 22;
+			int S = 17;
+
+			int W = DoSomething(L, S);
+			//int W = ((L + S) - 1) * L - S * 10;
+
+			System.Console.WriteLine(P);
+
+			System.Console.Write
+				(System.Environment.NewLine + "Press [ENTER] To Exit... ");
+
+			System.Console.ReadLine();
+		}
+	}
+}
---
+++ LEARNING_STEP_BY_STEP/Readme.txt
@@ -0,0 +1,105 @@
+﻿Namespace		Folder / Sub Directory
+Class			File
+
+--------------------------------------
+
+y = Sin(x)
+
+--------------------------------------
+
+OUTPUT	FunctionName	(	INPUT	)
+
+INPUT: Signature
+
+--------------------------------------
+
+Main: Startup Function
+
+Program: Startup Class
+
+--------------------------------------
+
+Notepad -> source.cs -> Code C# -> Save
+
+?> csc source.cs
+source.exe
+?>source.exe
+
+--------------------------------------
+
+Intelisence
+
+Namespace	-> {}
+Class		-> سه تا مستطیل خردلی رنگ که به هم متصل شده اند
+Function	-> یک مکعب خالی با خطوط صورتی
+
+--------------------------------------
+
+C:\X\Y\Z\T.exe
+
+D:\> C:\X\Y\Z\T.exe
+
+or
+
+Path C:\X\Y\Z;M:\N\P;...
+
+D:\> T.exe
+D:\> C:\X\Y\Z\T.exe
+
+--------------------------------------
+
+Console -> Error -> CTRL + . -> System.Console
+
+--------------------------------------
+
+Indentation
+
+CTRL + K + D -> Format The Whole Document
+
+--------------------------------------
+
+Select Single or Multiple Line(s):
+
+CTRL + K + C -> Comment
+CTRL + K + U -> Uncomment
+
+--------------------------------------
+
+Stack -> S
+Head  -> H
+
+--------------------------------------
+
+Variable Definition:
+
+	1. Inside of Function
+	2. Outsite of Function (Inside of Class)
+
+--------------------------------------
+
+Inside of Function
+
+این متغیرها، متغیرهایی هستند که در درون استک قرار می‌گیرند
+هر متغیری که در داخل استک، قرار گیرد یا اصطلاحا تعریف شود
+مطلقا مقدار اولیه ندارد
+camelCase
+
+--------------------------------------
+
+Variable Types:
+
+	1. Primitive Type (Value Type)
+	2. Reference Type
+
+--------------------------------------
+
+Primitive Type:
+
+متغیرهایی هستند که خودشان و مقدارشان در یک جا و در درون استک قرار می‌گیرند
+
+--------------------------------------
+
+F9 -> Breakpoint
+F10 -> Trace
+
+--------------------------------------
