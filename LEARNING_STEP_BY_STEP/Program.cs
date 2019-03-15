298b18b9ce8a09206346ffcb9ba75187d48c469c
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-03-15 13:28:37 +0330

    Making some changes!

--- LEARNING_STEP_BY_STEP/Program.cs
+++ LEARNING_STEP_BY_STEP/Program.cs
@@ -5,6 +5,19 @@
 //		static void Main(string[] args)
 //		{
 //			System.Console.WriteLine("Hello, World");
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			System.Console.WriteLine("Hello, World");
 
 //			System.Console.ReadLine();
 //		}
@@ -57,7 +70,7 @@
 
 //			int y;
 
-//			// y = x + 1; // Assignment!
+//			y = x + 1; // Assignment!
 
 //			System.Console.ReadLine();
 //		}
@@ -109,7 +122,12 @@
 //		{
 //			// تاکتیک
 //			//int x = 1;
-//			int firstNumber = 1;
+//			int firstNumber = 1; // camel Case
+
+//			// camel Case
+//			// UPPER_CASE
+//			// lower_case
+//			// Pascal Case
 
 //			System.Console.WriteLine(firstNumber);
 
@@ -143,8 +161,6 @@
 //	{
 //		static void Main(string[] args)
 //		{
-//			//int firstNumber = System.Console.ReadLine();
-
 //			string fullName = System.Console.ReadLine();
 
 //			System.Console.WriteLine(fullName);
@@ -216,7 +232,9 @@
 
 //			//string fullName = string.Format("Full Name: {0} {1}", firstName, lastName);
 
-//			//string fullName = string.Format("Full Name: {1} {0}", lastName, firstName);
+//			//string fullName = string.Format("Full Name: {1}, {0}", firstName, lastName);
+
+//			//string fullName = "Full Name: {firstName} {lastName}";
 
 //			//string fullName = $"Full Name: {firstName} {lastName}";
 
@@ -240,11 +258,12 @@
 //	{
 //		static void Main(string[] args)
 //		{
-//			int A = 10;
-//			int B = 20;
-//			int C = 30;
+//			//int A = 10;
+//			//int B = 20;
+//			//int C = 30;
 
-//			string result = string.Format("M = {1}, N = {0}, P = {1}", A, B, C);
+//			//string result =
+//			//	string.Format("A = {1}, B = {0}, C = {1}", A, B, C);
 
 //			System.Console.WriteLine(result);
 
@@ -261,9 +280,130 @@
 //	{
 //		static void Main(string[] args)
 //		{
-//			int index = 1;
+//			int index;
+
+//			index = 1;
 
 //			if (index <= 10)
+//			{
+//				System.Console.WriteLine("Hello, World!");
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		/// <summary>
+//		/// Learning Trace!
+//		/// </summary>
+//		static void Main(string[] args)
+//		{
+//			int index;
+
+//			index = 1;
+
+//			if (index <= 10)
+//			{
+//				System.Console.WriteLine("Hello, World!");
+//			}
+
+//			if(index > 20)
+//			{
+//				System.Console.WriteLine("How are you?");
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int index;
+
+//			index = 1;
+
+//			if (index <= 10)
+//			{
+//				System.Console.WriteLine("Hello, World!");
+//			}
+//			// در غیر اینصورت
+//			else
+//			{
+//				System.Console.WriteLine("How are you?");
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int index;
+
+//			index = 1;
+
+//			// دستور ذیل حرفه‌ای نیست
+//			//if (index <= 10)
+//			//	System.Console.WriteLine("Hello, World!");
+
+//			// دستور ذیل حرفه‌ای نیست
+//			//if (index <= 10) System.Console.WriteLine("Hello, World!");
+
+//			// دستور ذیل حرفه‌ای است
+//			if (index <= 10)
+//			{
+//				System.Console.WriteLine("Hello, World!");
+//			}
+
+//			if (index <= 10)
+//			{
+//				System.Console.WriteLine("Hello, World!");
+//				System.Console.WriteLine("Hello, World!");
+//				System.Console.WriteLine("Hello, World!");
+//				System.Console.WriteLine("Hello, World!");
+//				System.Console.WriteLine("Hello, World!");
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int index;
+
+//			index = 1;
+
+//			// Infinitive Loop
+//			while (index <= 10)
 //			{
 //				System.Console.WriteLine("Hello, World!");
 //			}
@@ -286,6 +426,8 @@
 //			while (index <= 10)
 //			{
 //				System.Console.WriteLine("Hello, World!");
+
+//				index = index + 1;
 //			}
 
 //			System.Console.ReadLine();
@@ -313,9 +455,43 @@
 //				// حاصل آنرا مجددا در همان متغیر ایندکس ذخیره می‌کنیم
 
 //				// جمله قشنگ و عامیانه
-//				// یک واحد به متغیر ایندکس اضافه می‌کند
+//				// یک واحد به متغیر ایندکس اضافه می‌کنیم
 //				index = index + 1;
 //			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int index = 1;
+
+//			index = index + 10;
+//			index += 10;
+
+//			index = index + 1;
+//			index += 1;
+//			index++;
+//			++index;
+
+//			index = index - 1;
+//			index -= 1;
+//			index--;
+//			--index;
+
+//			int A = 5;
+//			int B;
+
+//			B = ++A; // B = 6, A = 6
+//			B = A++; // B = 5, A = 6;
 
 //			System.Console.ReadLine();
 //		}
@@ -338,36 +514,8 @@
 //			{
 //				// Do Something(s)
 
-//				index = index + 1;
+//				index++;
 //			}
-
-//			System.Console.ReadLine();
-//		}
-//	}
-//}
-
-
-
-//namespace LEARNING_STEP_BY_STEP
-//{
-//	class Program
-//	{
-//		static void Main(string[] args)
-//		{
-//			int A = 10;
-
-//			A = A + 20;
-//			A += 20;
-
-//			A = A + 1;
-//			A += 1;
-//			A++;
-//			++A;
-
-//			A = A - 1;
-//			A -= 1;
-//			A--;
-//			--A;
 
 //			System.Console.ReadLine();
 //		}
@@ -387,6 +535,28 @@
 //			while (index <= 10)
 //			{
 //				// Do Something(s)
+
+//				index++;
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int index = 1;
+
+//			while (index <= 10)
+//			{
+//				System.Console.WriteLine("Hello, World!");
 
 //				index++;
 //			}
@@ -426,9 +596,111 @@
 //{
 //	class Program
 //	{
+//		/// <summary>
+//		/// نمایش اعداد طبیعی از یک تا پنجاه
+//		/// </summary>
 //		static void Main(string[] args)
 //		{
-//			int number = 10;
+//			int number = 50;
+
+//			for (int index = 1; index <= number; index++)
+//			{
+//				System.Console.WriteLine(index);
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		/// <summary>
+//		/// نمایش اعداد طبیعی فرد از یک تا پنجاه
+//		/// </summary>
+//		static void Main(string[] args)
+//		{
+//			int number = 50;
+
+//			for (int index = 1; index <= number; index += 2)
+//			{
+//				System.Console.WriteLine(index);
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		/// <summary>
+//		/// نمایش اعداد طبیعی زوج از دو تا پنجاه
+//		/// </summary>
+//		static void Main(string[] args)
+//		{
+//			int number = 50;
+
+//			for (int index = 2; index <= number; index += 2)
+//			{
+//				System.Console.WriteLine(index);
+//			}
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		/// <summary>
+//		/// تاکتیک‌های نام‌گذاری متغیرها
+//		/// </summary>
+//		static void Main(string[] args)
+//		{
+//			//int A;
+//			//string B;
+
+//			int firstNumber;
+//			int firstNumberInt;
+
+//			string fullName;
+//			string fullNameString;
+
+//			// دو استاندارد ذیل منسوخ شده است
+//			//int intFirstNumber;
+//			//string strFullName;
+
+//			System.Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+//namespace LEARNING_STEP_BY_STEP
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			System.Console.Write("choose a number: ");
+//			string numberString = System.Console.ReadLine();
+
+//			int number =
+//				System.Convert.ToInt32(numberString);
 
 //			for (int index = 1; index <= number; index++)
 //			{
@@ -448,17 +720,28 @@
 //	{
 //		static void Main(string[] args)
 //		{
-//			//int A;
-//			//int A;
+//			System.Console.Write("choose a number: ");
+//			string numberString = System.Console.ReadLine();
 
-//			//int A;
-//			//string A;
+//			int number =
+//				System.Convert.ToInt32(numberString);
 
-//			int firstNumber;
-//			int firstNumberInt;
+//			for (int index = 1; index <= number; index++)
+//			{
+//				System.Console.WriteLine(index);
+//			}
 
-//			string fullName;
-//			string fullNameString;
+//			//System.Console.WriteLine("Press [ENTER] To Exit... ");
+
+//			//System.Console.Write("Press [ENTER] To Exit... ");
+
+//			//System.Console.WriteLine();
+//			//System.Console.Write("Press [ENTER] To Exit... ");
+
+//			//System.Console.Write("\r\nPress [ENTER] To Exit... ");
+
+//			System.Console.Write
+//				(System.Environment.NewLine + "Press [ENTER] To Exit... ");
 
 //			System.Console.ReadLine();
 //		}
@@ -490,21 +773,6 @@
 //				$"The sum of these numbers is { sum }";
 
 //			System.Console.WriteLine(result);
-
-//			//System.Console.WriteLine("Press [ENTER] To Exit... ");
-
-//			//System.Console.Write("Press [ENTER] To Exit... ");
-
-//			//System.Console.Write("\r\nPress [ENTER] To Exit... ");
-
-//			//System.Console.Write("\r\n\r\nPress [ENTER] To Exit... ");
-
-//			//System.Console.WriteLine();
-//			//System.Console.WriteLine();
-//			//System.Console.Write("Press [ENTER] To Exit... ");
-
-//			//System.Console.Write
-//			//	(System.Environment.NewLine + System.Environment.NewLine + "Press [ENTER] To Exit... ");
 
 //			System.Console.Write
 //				(System.Environment.NewLine + "Press [ENTER] To Exit... ");
@@ -563,8 +831,8 @@
 //	{
 //		static int DoSomething(int x, int y)
 //		{
-//			//int result = ((x + y) - 1) * x - y * 10;
-//			int result = ((x + y) - 1) * x - y * 11;
+//			int result = ((x + y) - 1) * x - y * 10;
+//			//int result = ((x + y) - 1) * x - y * 11;
 
 //			return result;
 //		}
@@ -808,8 +1076,10 @@ namespace LEARNING_STEP_BY_STEP
 	{
 		static int GetRandomNumberBetween(int minValue, int maxValue)
 		{
-			System.Random random =
-				new System.Random(Seed: System.DateTime.Now.Millisecond);
+			System.Random random = new System.Random();
+
+			//System.Random random =
+			//	new System.Random(Seed: System.DateTime.Now.Millisecond);
 
 			int randomNumber =
 				random.Next(minValue: minValue, maxValue: maxValue);
